b3ea1f61043fd988b2c837994d285f7d90cbf6d3
Artem <48939302+rewwea@users.noreply.github.com>
2025-01-21 11:44:13 +0500

    Ispravil quicksort

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,32 +1,47 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <stack>
 using namespace std;
 
 void quickSort(unsigned int* arr, int low, int high) {
-    if (low < high) {
-        unsigned int pivot = arr[high];
-        int i = (low - 1);
-        for (int j = low; j <= high - 1; j++) {
-            if (arr[j] < pivot) {
-                i++;
-                unsigned int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-        unsigned int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
-        int pi = i + 1;
+    stack<int> s;
+    s.push(low);
+    s.push(high);
 
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+    while (!s.empty()) {
+        high = s.top(); s.pop();
+        low = s.top(); s.pop();
+
+        if (low < high) {
+            unsigned int pivot = arr[high];
+            int i = (low - 1);
+            for (int j = low; j <= high - 1; j++) {
+                if (arr[j] < pivot) {
+                    i++;
+                    unsigned int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            unsigned int temp = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = temp;
+            int pi = i + 1;
+
+            // Push left side to stack
+            s.push(low);
+            s.push(pi - 1);
+            
+            // Push right side to stack
+            s.push(pi + 1);
+            s.push(high);
+        }
     }
 }
 
 int main() {
-    unsigned int n = 1000; 
+    unsigned int n = 1000;
     unsigned int* arr = new unsigned int[n];
     srand(time(0));
 
